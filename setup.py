35740a78e4ed5965983b0bccb1b29c5bcd11bb81
boguta_m <boguta_m@epitech.eu>
2014-09-12 11:16:44 +0200

    added Setup and License for PyPI push

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# setup.py setup script
+
+from distutils.core import setup
+
+setup(name= "Shiba",
+      packages= ["Shiba"],
+      version = "1.0",
+      description= "A Python API for PriceMinister WebServices",
+      author= "Maxime Boguta",
+      author_email= "maxime.boguta@epitech.eu",
+      url= "https://github.com/ShibaAPI/shiba",
+      download_url= "https://github.com/ShibaAPI/shiba/tarball/1.0",
+      keywords= ["api", "priceminister", "python", "webservices"],
+      classifiers= [])
