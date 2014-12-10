05a730cc1bb568cdc89fa1135bd991df230f07c0
boguta_m <boguta_m@epitech.eu>
2014-12-10 11:21:31 +0100

    new tests

---
+++ tests/online/test_accountingmanagement.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Class AccountingManagementTest
+# Testing AccountingManagement Class methods
+# https://developer.priceminister.com/blog/fr/documentation/accounting
+
+
+from __future__ import unicode_literals
+
+from Shiba.accountingmanagement import AccountingManagement
+from Shiba.shibaconnection import ShibaConnection
+from Shiba.shibaexceptions import *
+
+import os
+import ConfigParser
+
+import unittest
+
+from datetime import date
+
+
+class AccountingManagementTest(unittest.TestCase):
+
+    def setUp(self):
+        settings = ConfigParser.ConfigParser()
+        try:
+            settings.read(os.path.dirname(os.path.realpath(__file__)) + "/Assets/nosetests.cfg")
+        except:
+            raise ShibaCallingError("error : can't read login ID from the nosetests.cfg file")
+        try:
+            login = settings.get(str("NoseConfig"), "login")
+            pwd = settings.get(str("NoseConfig"), "pwd")
+        except:
+            raise ShibaCallingError("error : configuration file doesn't seem to be regular")
+        self.init = AccountingManagement(ShibaConnection(login, pwd, "https://ws.sandbox.priceminister.com"))
+
+    def test_get_operations(self):
+        """get_operations routine test, with date object as lastoperationdate too"""
+        obj = self.init.get_operations()
+        self.assertTrue("getoperationsresult" in obj.content.tag)
+        obj = self.init.get_operations("21/12/2012-00:00:00")
+        self.assertTrue(obj.content.request.lastoperationdate == "21/12/2012-00:00:00")
+        testdate = date(2012, 12, 21)
+        obj = self.init.get_operations(testdate)
+        self.assertTrue(obj.content.request.lastoperationdate == "21/12/12-00:00:00")
+        obj = None
+        try:
+            obj = self.init.get_operations("INVALIDDATE")
+        except ShibaParameterError:
+            pass
+        self.assertTrue(obj is None)
+
+    def test_get_compensation_details(self):
+        """get_compensation_details test, must fail"""
+        obj = None
+        try:
+            obj = self.init.get_compensation_details("1337")
+        except ShibaParameterError:
+            pass
+        self.assertTrue(obj is None)
---
+++ tests/online/test_inventorymanagement.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Class InventoryManagementTest
+# Testing InventoryManagement Class methods
+# https://developer.priceminister.com/blog/fr/documentation/inventory-management
+
+
+from __future__ import unicode_literals
+
+from Shiba.inventorymanagement import InventoryManagement
+from Shiba.shibaconnection import ShibaConnection
+from Shiba.shibaexceptions import *
+from nose.tools import *
+
+import xmltodict
+from lxml import objectify
+
+import unittest
+
+import ConfigParser
+import os
+
+class InventoryManagementTest(unittest.TestCase):
+
+    def setUp(self):
+        settings = ConfigParser.ConfigParser()
+        try:
+            settings.read(os.path.dirname(os.path.realpath(__file__)) + "/Assets/nosetests.cfg")
+        except:
+            raise ShibaCallingError("error : can't read login ID from the nosetests.cfg file")
+        try:
+            login = settings.get(str("NoseConfig"), "login")
+            pwd = settings.get(str("NoseConfig"), "pwd")
+        except:
+            raise ShibaCallingError("error : configuration file doesn't seem to be regular")
+        self.init = InventoryManagement(ShibaConnection(login, pwd, "https://ws.sandbox.priceminister.com"))
+
+    def test_product_types(self):
+        """product_types return test"""
+
+        ptypes = self.init.product_types()
+        self.assertTrue("producttypesresult" in ptypes.content.tag)
+
+    def test_product_type_template(self):
+        """product_type_template tests on two scopes, for a fixed alias, plus a fail result"""
+
+        alias = "insolites_produit"
+        ptemplate = self.init.product_type_template(alias, "")
+        self.assertTrue("producttypetemplateresult" in ptemplate.content.tag)
+        ptemplate = self.init.product_type_template(alias, "VALUES")
+        self.assertTrue("producttypetemplateresult" in ptemplate.content.tag)
+
+    @raises(ShibaParameterError)
+    def test_product_type_template_fail(self):
+        self.init.product_type_template("INVALIDALIAS", "INVALIDSCOPE")
+
+    def test_generic_import_file(self):
+        """generic_import_file test, from an XML file. Conversion is done by xmltodict from a dict or OrderedDict
+        , as well with objectify with an objectified ElementTree element"""
+
+        f = open(os.path.dirname(os.path.realpath(__file__)) + "/Assets/genericimportfile.xml", "rb")
+        testdict = xmltodict.parse(f)
+        ret = self.init.generic_import_file(testdict)
+        self.assertTrue("OK" == ret.content.response.status)
+        f = open(os.path.dirname(os.path.realpath(__file__)) + "/Assets/genericimportfile.xml", "rb")
+        testobj = objectify.parse(f)
+        ret = self.init.generic_import_file(testobj)
+        self.assertTrue("OK" == ret.content.response.status)
+
+    def test_generic_import_report(self):
+        """genreic_import_report method test from an import file call"""
+        f = open(os.path.dirname(os.path.realpath(__file__)) + "/Assets/genericimportfile.xml", "rb")
+        testobj = objectify.parse(f)
+        ret = self.init.generic_import_file(testobj)
+        importid = ret.content.response.importid
+        ret = self.init.generic_import_report(importid)
+        self.assertTrue("file" == ret.content.response.file.filename)
+
+    def test_get_available_shipping_types(self):
+        try:
+            self.init.get_available_shipping_types()
+        except ShibaRightsError:
+            pass
+
+    def test_export_inventory(self):
+        obj = self.init.export_inventory()
+        self.assertTrue("inventoryresult" in obj.content.tag)
