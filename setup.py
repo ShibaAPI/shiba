23f9775c0594a6e354e5ddb3471ff1df19c35cd3
Salem Harrache <salem.harrache@abc-culture.fr>
2016-07-13 17:28:27 +0200

    Improved setuptools install script

--- setup.py
+++ setup.py
@@ -1,17 +1,57 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# setup.py setup script
+import re
+import os.path as op
 
-from setuptools import setup
 
-setup(name="Shiba",
-      packages=["shiba"],
-      version="1.1.5-dev",
-      description="A Python API for PriceMinister WebServices",
-      author="Maxime Boguta",
-      author_email="maxime.boguta@epitech.eu",
-      url="https://github.com/ShibaAPI/shiba",
-      download_url="https://github.com/Phylante/shiba/tarball/1.1.5-dev",
-      keywords=["api", "priceminister", "python", "webservices"],
-      install_requires=["requests", "xmltodict", "lxml", "nose"],)
+from codecs import open
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    ''' Return the file content. '''
+    here = op.abspath(op.dirname(__file__))
+    with open(op.join(here, fname), 'r', 'utf-8') as fd:
+        return fd.read()
+
+readme = read('README.rst')
+
+
+requirements = [
+    "requests",
+    "xmltodict",
+    "lxml",
+    "nose"
+]
+
+version = ''
+version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                    read(op.join('shiba', '__init__.py')),
+                    re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+
+setup(
+    name='Shiba',
+    author="Maxime Boguta",
+    author_email="maxime.boguta@epitech.eu",
+    version=version,
+    url='https://github.com/ShibaAPI/shiba',
+    packages=find_packages(),
+    install_requires=requirements,
+    zip_safe=True,
+    description="A Python API for PriceMinister WebServices",
+    long_description=readme,
+    keywords=["api", "priceminister", "python", "webservices"],
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
